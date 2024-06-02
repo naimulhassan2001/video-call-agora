c66ad49cdd604da8d1392b1d49b58f949149f54f
naimulhassan <developernaimul00@gmail.com>
2024-06-02 14:00:05 +0600

    init

---
+++ lib/utils/secret.dart
@@ -0,0 +1,5 @@
+
+const appId = "8bd57b59e78e480cab86ef429dcf57de";
+const token =
+    "007eJxTYGBf28Xp/PL/bJ1Xn0T6b+3RYHX4cPn1wvkBmfe4/He/ucenwGCRlGJqnmRqmWpukWpiYZCcmGRhlppmYmSZkpxmap6SmsoTk9YQyMhg0iTPysgAgSA+F0NuZXxyRmJeXmoOAwMAjAIiuQ==";
+const channel = "my_channel";
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import agora_rtc_engine
+import iris_method_channel
+import path_provider_foundation
+import wakelock_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
+  IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+}
